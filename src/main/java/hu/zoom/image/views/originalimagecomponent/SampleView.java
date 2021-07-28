225b0974f6cd2e67ad36b34d7718569283b5c327
dpisti17 <d-pisti@hotmail.com>
2021-07-28 14:01:36 +0200

    update
    this is what i need

--- src/main/java/hu/zoom/image/views/originalimagecomponent/SampleView.java
+++ src/main/java/hu/zoom/image/views/originalimagecomponent/SampleView.java
@@ -20,17 +20,18 @@ public class SampleView extends VerticalLayout {
 
 	public SampleView() throws IOException {
 		addClassName("original-imagecomponent-view");
-		try (InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("file.jpg")) {
-			Image imageFromFile = new Image(new StreamResource("originalFile", () -> inputStream), "FROM FILE");
-			add(imageFromFile);
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("file.jpg");
+		Image imageFromFile = new Image(new StreamResource("originalFile", () -> inputStream), "FROM FILE");
+		add(imageFromFile);
 
-			Image imageFromUrl = new Image("https://fortepan.download/_photo/1600/fortepan_85636.jpg", "FROM URL");
-			add(imageFromUrl);
-		}
+		Image imageFromUrl = new Image("https://fortepan.download/_photo/1600/fortepan_85636.jpg", "FROM URL");
+		add(imageFromUrl);
 
-		// i need these sample work as zoomable image.
+		// i need these sample work as zoomable image like here:
 		// https://openseadragon.github.io
-		// Here is the main js component what should be implemented as:
+		// the webjar: https://github.com/webjars/openseadragon
+
+		// The javaScript component should be implemented as:
 		// -- com.vaadin.flow.component.littemplate.LitTemplate OR
 		// -- com.vaadin.flow.component.polymertemplate.PolymerTemplate
 
